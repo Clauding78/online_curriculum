110df2ddee93b9a805f74b7a54ee56a71632a4b5
Clauding78 <henrique_miranda_work_c937@proton.me>
2026-09-25 23:59:04 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx';
 
 import './main.css';
 
+import './js/language.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
