b39fa95ebac0141dfb012455c1dadc9e36097e51
Clauding78 <henrique_miranda_work_c937@proton.me>
2026-09-26 00:22:26 +0000

    Update language.js

--- src/js/language.js
+++ src/js/language.js
@@ -61,7 +61,7 @@
 
         if (_cache[_cacheKey]) return _cache[_cacheKey];
 
-        const _promise = fetch(`/src/public/lang/${lang}/${ns}.json`)
+        const _promise = fetch(`/online_curriculum/src/public/lang/${lang}/${ns}.json`)
             .then ((res) => {
                 if (!res.ok) throw new Error(`ERROR - File missing "/client/public/lang/${lang}/${ns}.json" ...`);
                 return res.json();
