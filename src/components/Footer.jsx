318b471845a2665502c3510ea740f3e162715a77
Clauding78 <henrique_miranda_work_c937@proton.me>
2026-09-26 13:22:50 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,77 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <footer>
-            <span>footer</span>
+        <footer data-i18n-ns='main/footer'>
+            <section className='Footer_top_section'>
+                <div className='Container_Footer_mySelf'>
+                    <div className='Footer_mySelf_text'>
+                        <span data-i18n='footer.footer_text'></span>
+                    </div>
+
+                    <div className='Footer_site_title'>
+                        <span>mySelf</span>
+                    </div>
+                </div>
+                
+                <ul className='Footer_social_media_icon_links'>
+                    <li><a href=''><img src='' /></a></li>
+                    <li><a href=''><img src='' /></a></li>
+                    <li><a href=''><img src='' /></a></li>
+                    <li><a href=''><img src='' /></a></li>
+                    <li><a href=''><img src='' /></a></li>
+                </ul>
+            </section>
+
+            <section>
+                <ul className='Footer_links_section_ul'>
+                    <li className='FLS_block'>
+                        <span data-i18n='footer.LS_1st_block_title' className='FLS_block_title'></span>
+                        <ul>
+                            <li><a data-i18n='footer.LS_1st_block__1_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_1st_block__2_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_1st_block__3_link' href=''></a></li>
+                        </ul>
+                    </li>
+
+                    <li className='FLS_block'>
+                        <span data-i18n='footer.LS_2nd_block_title' className='FLS_block_title'></span>
+                        <ul>
+                            <li><a data-i18n='footer.LS_2nd_block__1_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_2nd_block__2_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_2nd_block__3_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_2nd_block__4_link' href=''></a></li>
+                        </ul>
+                    </li>
+
+                    <li className='FLS_block'>
+                        <span data-i18n='footer.LS_3rd_block_title' className='FLS_block_title'></span>
+                        <ul>
+                            <li><a data-i18n='footer.LS_3rd_block__1_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_3rd_block__2_link' href='Projects/Websites/Websites.php'></a></li>
+                        </ul>
+                    </li>
+
+                    <li className='FLS_block'>
+                        <span data-i18n='footer.LS_4th_block_title' className='FLS_block_title'></span>
+                        <ul>
+                            <li><a data-i18n='footer.LS_4th_block__1_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_4th_block__2_link' href=''></a></li>
+                            <li><a data-i18n='footer.LS_4th_block__3_link' href=''></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </section>
+
+            <section className='Footer_bottom_section'>
+                <ul className='Footer_bottom_links'>
+                    <li><a data-i18n='footer.LD_1st_link' href=''></a></li>
+                    <li><a data-i18n='footer.LD_2nd_link' href=''></a></li>
+                    <li><a data-i18n='footer.LD_3rd_link' href=''></a></li>
+                    <li><a data-i18n='footer.LD_4th_link' href=''></a></li>
+                </ul>
+
+                <span className='Footer_copyright'>Copyright &copy 2026 - future, internet anon</span>
+            </section>
         </footer>
     );
 }
