bf7fc36a262e59267da23384a246b5822977b139
Clauding78 <henrique_miranda_work_c937@proton.me>
2026-09-26 13:21:21 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import Layout from './components/Layout.jsx';
+import Layout from './components/Layout';
 
-import Home from './pages/Home.jsx';
-import Habilities from './pages/Habilities.jsx';
-import Contact from './pages/Contact.jsx';
+import Home from './pages/Home';
+import Habilities from './pages/Habilities';
+import Resources from './pages/Resources';
+import FAQs from './pages/FAQs';
+import Contact from './pages/Contact';
 
-export default function App() {
+function App() {
     return (
-        <HashRouter>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/habilities' element={<Habilities />} />
-                    <Route path='/contact' element={<Contact />} />
-                </Route>
-            </Routes>
-        </HashRouter>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path='/' element={<Home />} />
+                <Route path='/habilities' element={<Habilities />} />
+                <Route path='/resources' element={<Resources />} />
+                <Route path='/faqs' element={<FAQs />} />
+                <Route path='/contact' element={<Contact />} />
+            </Route>
+        </Routes>
     );
 }
+
+export default App;
